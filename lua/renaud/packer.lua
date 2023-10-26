c025dde97ffd5a3e6e914c085375aab11a529505
Renald Morino <renaudmorin2001@gmail.com>
2023-10-25 20:54:45 -0400

    Adds markdown preview plugin

--- lua/renaud/packer.lua
+++ lua/renaud/packer.lua
@@ -56,4 +56,6 @@ return require('packer').startup(function(use)
   }
 }
 
+use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, }) 
+
 end)
